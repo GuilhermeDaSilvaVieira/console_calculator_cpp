7211259bb72e0730effce704f07aa5228e9d3174
Sangrito <guilhermedas.vieira@gmail.com>
2021-10-28 16:21:02 -0300

    primeiros passos

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+// OBJETIVO
+// Realizar calculo com entrada do usuário e as 4 operações
+//
+// REQUISITOS
+// Interface simples e aconchegante
+// Ordem de entrada, número, operador, número
+// Números decimais
+int main()
+{
+  std::cout << "Olá mundo!\n";
+  return EXIT_SUCCESS;
+}
